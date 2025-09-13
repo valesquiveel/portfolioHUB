58cebf6bc785af7ddf5d34b7d8ed6548ce86e3ff
valesquiveel <139177805+valesquiveel@users.noreply.github.com>
2025-09-13 00:07:08 +0000

    att 2

---
+++ Js/index.js
@@ -0,0 +1,20 @@
+const sections = document.querySelectorAll('.section-animate');
+
+        const observerOptions = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.2
+        };
+
+        const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('is-visible');
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, observerOptions);
+
+        sections.forEach(section => {
+            observer.observe(section);
+        });
